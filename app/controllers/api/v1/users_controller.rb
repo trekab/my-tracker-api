2747eaaa9000dfbe120f00f35cc8c2f5dd7771f3
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-07 01:18:20 +0300

    Add update action for the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
 
   # GET /users/1
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   # POST /users
@@ -16,10 +17,23 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
     # Only allow a trusted parameter "white list" through.
     def user_params
       params.require(:user).permit(:email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: %i[show create] 
+      resources :users, only: %i[show create update] 
     end
   end
 end
