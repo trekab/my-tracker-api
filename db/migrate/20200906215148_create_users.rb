b05663871debfcd215758b64f7fa8e711e8cdaca
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-07 01:00:48 +0300

    Create user model

---
+++ db/migrate/20200906215148_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.index :email, unique: true
+      t.string :password_digest, null: false
+      t.boolean :admin, default: false
+
+      t.timestamps
+    end
+  end
+end
