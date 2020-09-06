b890c305fb97b7298c2f5147e6ed2a260d677924
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-07 01:23:49 +0300

    Setup JWT gem

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module MyTrackerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
