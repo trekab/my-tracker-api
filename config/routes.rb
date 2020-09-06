32b0018fec03c58d2bfbd43bc89de9a381877684
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-07 00:49:39 +0300

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Api definition
+  namespace :api, defaults: { format: :json } do
+    # We are going to list our resources here
+  end
 end
