ee53afcf38ba7d5b45b67733ccff1fa8e581d680
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-07 03:30:13 +0300

    Add the products create, update and destroy actions

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -8,4 +8,9 @@ module Authenticable
     decoded = JsonWebToken.decode(header)
     @current_user = User.find(decoded[:user_id]) rescue ActiveRecord::RecordNotFound
   end
+
+  protected
+  def check_login
+    head :forbidden unless self.current_user
+  end
 end
--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -17,4 +17,57 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body)
     assert_equal @product.title, json_response['title']
   end
+
+  test 'should create product' do
+    assert_difference('Product.count') do
+      post api_v1_products_url,
+        params: { product: { title: @product.title, price: @product.price, published: @product.published } },
+        headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+        as: :json
+    end
+    assert_response :created
+  end
+
+  test 'should forbid create product' do
+    assert_no_difference('Product.count') do
+      post api_v1_products_url,
+        params: { product: { title: @product.title, price: @product.price, published: @product.published } },
+        as: :json
+    end
+    assert_response :forbidden
+  end
+
+  test 'should update product' do
+    patch api_v1_product_url(@product),
+      params: { product: { title: @product.title } },
+      headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+      as: :json
+    assert_response :success
+  end
+
+  test 'should forbid update product' do
+    patch api_v1_product_url(@product),
+      params: { product: { title: @product.title } },
+      headers: { Authorization: JsonWebToken.encode(user_id: users(:two).id) },
+      as: :json
+    assert_response :forbidden
+  end
+
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      delete api_v1_product_url(@product), 
+        headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+        as: :json
+    end
+    assert_response :no_content
+  end
+
+  test "should forbid destroy product" do
+    assert_no_difference('Product.count') do
+      delete api_v1_user_url(@product), 
+        headers: { Authorization: JsonWebToken.encode(user_id: users(:two).id) },
+        as: :json
+    end
+    assert_response :forbidden
+  end
 end
