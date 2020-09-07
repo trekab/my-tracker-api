020cb3e96dc57e15418eb5ab1640a8c1df5d5c75
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-07 03:09:12 +0300

    Add some validations to products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  test 'destroy user should destroy linked product' do
-    assert_difference('Product.count', -1) do
-      users(:one).destroy
-    end
+  test "should have a positive price" do
+    product = products(:one)
+    product.price = -1
+    assert_not product.valid?
   end
 end
