ab87705bda0a0518f02bcb4b2e783054bedef5c6
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-07 17:37:38 +0300

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json } do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Product.delete_all
 User.delete_all
 
