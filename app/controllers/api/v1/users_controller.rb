c4bcdfc825c30e2d40ef9ab183b7920d7501516b
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-07 03:00:18 +0300

    Restrict actions for unauthorized users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
+  before_action :check_owner, only: %i[update destroy]
 
   # GET /users/1
   def show
@@ -41,5 +42,9 @@ class Api::V1::UsersController < ApplicationController
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def check_owner
+      head :forbidden unless @user.id == current_user&.id
     end
 end
