9fdadc5fbc1eb2ca2875d767e143070c687c5e37
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-16 20:08:50 +0300

    Remove unnecessary presence validation for password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
-  validates :password_digest, presence: true
 
   has_secure_password
   has_many :products, dependent: :destroy
